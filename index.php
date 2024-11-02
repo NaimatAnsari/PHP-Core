b3f8fefd20381aa9f6e3a7eef8211e70fba52248
Naimat Ansari <naimatansari31@gmail.com>
2024-11-02 20:15:36 +0500

    add

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
 
     echo $print . $cr;
 
+    $a = 10;
+    $b = 90;
+
+    $result = $a + $b ; 
+
+    echo $result;
+
+    $naimat = 100 ;
+    $fahad = 100 ;
+
+    $sum = $naimat === $fahad ;
+    echo $sum ;
 ?>
