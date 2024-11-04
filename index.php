a80bb3ce1411049c97e503247aa0f5ace8950305
Naimat Ansari <naimatansari31@gmail.com>
2024-11-05 00:24:21 +0500

    add

--- index.php
+++ index.php
@@ -1,89 +1,79 @@
 <?php
-
     $print = "Hello PHP World";  
     $cr = 7;
 
-    echo $print . $cr;
+    echo $print . " " . $cr . "<br>";
 
     $a = 10;
     $b = 90;
+    $result = $a + $b; 
+    echo "Result: $result<br>";
 
-    $result = $a + $b ; 
+    $naimat = 100;
+    $fahad = 100;
+    $sum = $naimat === $fahad;
+    echo "Sum: " . ($sum ? "true" : "false") . "<br>";
 
-    echo $result;
-
-    $naimat = 100 ;
-    $fahad = 100 ;
-
-    $sum = $naimat === $fahad ;
-    echo $sum ;
-
-    $x = 8 ;
-
+    $x = 8;
     $y = $x % 2 == 0;
-
-    echo $y;
+    echo $y ? "Even<br>" : "Odd<br>";
 
     $a = 10;
     $b = 20;
-
-    $c = $a + $b ;
-
-    $c ? 30 : 40 ;
-
-    echo $c ;
+    $c = $a + $b;
+    echo $c . "<br>";
 
     if ($c == 30) {
-        echo "Yes This is Code $c" ;
+        echo "Yes This is Code $c<br>";
     } else {
-       echo "try Again $c" ;
+        echo "Try Again $c<br>";
     }
 
-    $age = 18 ;
-
+    $age = 18;
     if ($age >= 18) {
-        echo "Yes You can Vote $age" ;
+        echo "Yes You can Vote $age<br>";
     } else {
-        echo "No You can't Vote $age" ;
+        echo "No You can't Vote $age<br>";
     }
 
     $hasID = false;
-
     if ($age >= 18 || $hasID) {
-        echo "Yes You Can Enter The Club Because Your Age is $age";
+        echo "Yes You Can Enter The Club Because Your Age is $age<br>";
     } else {
-        echo "No You Can't Enter The Club Because You Are Under 18";
+        echo "No You Can't Enter The Club Because You Are Under 18<br>";
     }
-    
-    $day = 3 ;
 
+    $day = 3;
     switch ($day) {
         case 1:
-            echo "Monday";
+            echo "Monday<br>";
             break;
-            case 2:
-                echo "Tuesday";
-                break;
-                case 3:
-                    echo "Wednesday";
-                    break;
-                    case 4:
-                        echo "Thursday";
-                        break;
-                        case 5:
-                            echo "Friday";
-                            break;
-                            case 6:
-                                echo "Saturday";
-                                break;
-                                case 7:
-                                    echo "Sunday";
-                                    break;
-                                default:
-                                    echo "Invalid Day";
-                                break;
+        case 2:
+            echo "Tuesday<br>";
+            break;
+        case 3:
+            echo "Wednesday<br>";
+            break;
+        case 4:
+            echo "Thursday<br>";
+            break;
+        case 5:
+            echo "Friday<br>";
+            break;
+        case 6:
+            echo "Saturday<br>";
+            break;
+        case 7:
+            echo "Sunday<br>";
+            break;
+        default:
+            echo "Invalid Day<br>";
+            break;
     }
-    
 
-
+    $number = 2;
+    for ($i = 1; $i <= 10; $i++) { 
+        $table = $number * $i;
+        echo "$number x $i = $table <br>";
+    }
 ?>
