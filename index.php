5f773dce2b8dd32a267ebee5928f12bd8f1731f9
Naimat Ansari <naimatansari31@gmail.com>
2024-11-04 21:28:28 +0500

    add

--- index.php
+++ index.php
@@ -55,7 +55,34 @@
         echo "No You Can't Enter The Club Because You Are Under 18";
     }
     
-    
+    $day = 3 ;
+
+    switch ($day) {
+        case 1:
+            echo "Monday";
+            break;
+            case 2:
+                echo "Tuesday";
+                break;
+                case 3:
+                    echo "Wednesday";
+                    break;
+                    case 4:
+                        echo "Thursday";
+                        break;
+                        case 5:
+                            echo "Friday";
+                            break;
+                            case 6:
+                                echo "Saturday";
+                                break;
+                                case 7:
+                                    echo "Sunday";
+                                    break;
+                                default:
+                                    echo "Invalid Day";
+                                break;
+    }
     
 
 
