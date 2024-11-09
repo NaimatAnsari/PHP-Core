c03d1c8a30099972d6359325975724f9be17ae5c
Naimat Ansari <naimatansari31@gmail.com>
2024-11-09 20:28:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -98,4 +98,33 @@ $number = 100;
         $table = $number * $i;
         echo "$number x $i = $table <br>";
     }
+
+$day = 7;
+    switch ($day) {
+        case 1:
+            echo "Monday<br>";
+            break;
+        case 2:
+            echo "Tuesday<br>";
+            break;
+        case 3:
+            echo "Wednesday<br>";
+            break;
+        case 4:
+            echo "Thursday<br>";
+            break;
+        case 5:
+            echo "Friday<br>";
+            break;
+        case 6:
+            echo "Saturday<br>";
+            break;
+        case 7:
+            echo "Sunday<br>";
+            break;
+        default:
+            echo "Invalid Day<br>";
+            break;
+    }
+
 ?>
